ea270acb800139e372582653c2ae92e5941e0d7f
wanghuan-520 <huan.wang@aelf.io>
2024-12-24 22:04:31 +0800

    Add logic for querying agent execution results (#69) (#70)
    add result query API

--- src/AISmart.Domain.Shared/Agents/MockA/MockAAgentState.cs
+++ src/AISmart.Domain.Shared/Agents/MockA/MockAAgentState.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using AISmart.Application.Grains.Agents.MockA;
 using Orleans;
 
 namespace AISmart.Agents.MockA;
@@ -8,4 +9,9 @@ public class MockAAgentState : StateBase
 {
     [Id(0)] public List<string> ThreadIds { get; set; }
     [Id(1)] public int Number { get; set; }
+
+    public void Apply(MockAAddNumberEvent @event)
+    {
+        Number += 1;
+    }
 }
