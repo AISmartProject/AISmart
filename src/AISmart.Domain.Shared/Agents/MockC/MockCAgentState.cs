2b87403877ab9a41d74777d356543580339b72c5
wanghuan-520 <huan.wang@aelf.io>
2024-12-24 21:03:58 +0800

    add result query API

--- src/AISmart.Domain.Shared/Agents/MockC/MockCAgentState.cs
+++ src/AISmart.Domain.Shared/Agents/MockC/MockCAgentState.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using AISmart.Application.Grains.Agents.MockC;
 using Orleans;
 
 namespace AISmart.Agents.MockC;
@@ -8,4 +9,9 @@ public class MockCAgentState : StateBase
 {
     [Id(0)] public List<string> ThreadIds { get; set; }
     [Id(1)] public int Number { get; set; }
+    
+    public void Apply(MockCAddNumberEvent @event)
+    {
+        Number += 1;
+    }
 }
