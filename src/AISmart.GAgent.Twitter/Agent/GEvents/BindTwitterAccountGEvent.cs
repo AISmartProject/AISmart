78b51d560e5688c66cf793740669ad37ab210e1d
house1313 <house@schrodingernft.ai>
2024-12-28 15:52:34 +0800

    1.refine log. 2.add userName param.

--- src/AISmart.GAgent.Twitter/Agent/GEvents/BindTwitterAccountGEvent.cs
+++ src/AISmart.GAgent.Twitter/Agent/GEvents/BindTwitterAccountGEvent.cs
@@ -8,4 +8,5 @@ public class BindTwitterAccountGEvent : TweetGEvent
     [Id(0)] public string UserId { get; set; }
     [Id(1)] public string Token { get; set; }
     [Id(2)] public string TokenSecret { get; set; }
+    [Id(3)] public string UserName { get; set; }
 }
