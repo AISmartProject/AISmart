69e870332f6f42185695d4c4c69180d8cd1ccd8c
house1313 <house@schrodingernft.ai>
2025-01-07 10:58:53 +0800

    add BusinessAgentId

--- src/AISmart.GAgent.AtomicAgent/Models/AgentData.cs
+++ src/AISmart.GAgent.AtomicAgent/Models/AgentData.cs
@@ -6,5 +6,6 @@ public class AgentData
     [Id(0)] public Guid Id { get; set; }
     [Id(1)] public string Type { get; set; }
     [Id(2)] public string Name { get; set; }
-    [Id(3)] public string Properties { get; set; }
+    [Id(3)] public string BusinessAgentId { get; set; }
+    [Id(4)] public string Properties { get; set; }
 }
