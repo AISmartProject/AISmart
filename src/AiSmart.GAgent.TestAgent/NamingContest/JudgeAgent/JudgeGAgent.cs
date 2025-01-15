c6b89a3218c882c85ddc0c71e8b2fadd90796dd7
Jeff-Xu23 <jeffery.xu@aelf.io>
2025-01-15 22:26:12 +0800

    feat:modify judge agent

--- src/AiSmart.GAgent.TestAgent/NamingContest/JudgeAgent/JudgeGAgent.cs
+++ src/AiSmart.GAgent.TestAgent/NamingContest/JudgeAgent/JudgeGAgent.cs
@@ -161,7 +161,7 @@ public class JudgeGAgent : GAgentBase<JudgeState, JudgeCloneSEvent>, IJudgeGAgen
                 await PublishAsync(new VoteCharmingCompleteEvent()
                 {
                     Winner = winner,
-                    AgentId = this.GetPrimaryKey(),
+                    AgentId = GetRealJudgeId(),
                     Round = @event.Round
                 });
             }
